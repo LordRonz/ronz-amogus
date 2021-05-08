c37910e9bc55ac702e825268d21e63a2e878ce89
LordRonz <aarontanhar2000@gmail.com>
2021-05-08 15:44:32 +0700

    reddit sort by top

--- meme_handler.py
+++ meme_handler.py
@@ -4,15 +4,19 @@ from random import randint, choice
 SUBREDDITS = ['dankmemes', 'memes']
 
 def isValidImg(url):
-    return url.startswith('https://i.redd.it') and (url.endswith('.jpg') or url.endswith('.gif') or url.endswith('.png'))
+    return url.startswith('https://i.') and (url.endswith('.jpg') or url.endswith('.gif') or url.endswith('.png') or url.endswith('.gifv'))
 
 async def get_meme():
-    res = requests.get(f'https://www.reddit.com/r/{choice(SUBREDDITS)}/hot/.json?sort=hot&showmedia=true&mediaonly=true&is_self=true&limit=100', headers={'User-agent': 'Amogus'})
+    res = requests.get(f'https://www.reddit.com/r/{choice(SUBREDDITS)}/top/.json?sort=top&t=day&showmedia=true&mediaonly=true&is_self=true&limit=69', headers={'User-agent': 'ronz-amogus'})
 
     url = ''
 
+    json = res.json()['data']['children']
+    length = len(json) - 1
+    print(length)
+
     while not isValidImg(url):
-        index = randint(0, 101)
-        url = res.json()['data']['children'][index]['data']['url']
+        index = randint(0, length)
+        url = json[index]['data']['url']
 
     return url
