8bd494ff2b4be6765701f8cba0434b38ec3818eb
LordRonz <aarontanhar2000@gmail.com>
2021-05-11 14:29:13 +0700

    sus cooldown

--- cogs/text.py
+++ cogs/text.py
@@ -6,17 +6,27 @@ import requests
 class Text(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._cd = commands.CooldownMapping.from_cooldown(1, 3, commands.BucketType.guild)
+
 
         with open(f'./profanities/profanities.txt', 'r') as f:
             self.profanities = f.readlines()
 
         self.gooba_lyrics = requests.get('http://gist.githubusercontent.com/LordRonz/da8dcbf4cfdd07a19f239f5f6f555299/raw/4e9480c4a2fd8621dc9102b0e72ccf185575e95d/gooba.txt').text
 
+    def get_ratelimit(self, message):
+        '''Returns the ratelimit left'''
+        bucket = self._cd.get_bucket(message)
+        return bucket.update_rate_limit()
+
     @commands.Cog.listener('on_message')
     async def sus(self, message):
         if message.author == self.bot.user or message.author.bot or not message.guild:
             return
         if 'sus' in message.content.lower():
+            ratelimit = self.get_ratelimit(message)
+            if ratelimit:
+                return
             await flushed(message)
             await message.channel.send('**A M O G U S**')
 
