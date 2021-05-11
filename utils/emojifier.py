ac0501c2f2dbfe5585429df3c032cb1605c3f17c
LordRonz <aarontanhar2000@gmail.com>
2021-05-11 18:27:33 +0700

    emojify

---
+++ utils/emojifier.py
@@ -0,0 +1,25 @@
+SPECIALCODES = {
+    '0': ':zero:',
+    '1': ':one:',
+    '2': ':two:',
+    '3': ':three:',
+    '4': ':four:',
+    '5': ':five:',
+    '6': ':six:',
+    '7': ':seven:',
+    '8': ':eight:',
+    '9': ':nine:',
+    '#': ':hash:',
+    '*': ':asterisk:',
+    '?': ':grey_question:',
+    '!': ':grey_exclamation:',
+    ' ': '   '
+}
+
+async def get_emojified_text(msg: str):
+    if len(msg) > 69:
+        return 'Keep it under 69 characters fam'
+
+    msg = ' '.join(msg.split())
+
+    return ''.join(f':regional_indicator_{c}: ' if c.isalpha() else f'{SPECIALCODES[c]} ' if c in SPECIALCODES else '' for c in msg)
