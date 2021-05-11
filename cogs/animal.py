ad57532fc8fce25d90e01c43cec202645a92e243
LordRonz <aarontanhar2000@gmail.com>
2021-05-11 21:16:27 +0700

    some doggos

--- cogs/animal.py
+++ cogs/animal.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from utils.add_reaction import flushed
 from utils.cataas import get_cat
+from utils.dogaas import get_doggo
 
 class Animal(commands.Cog):
     def __init__(self, bot):
@@ -14,6 +15,15 @@ class Animal(commands.Cog):
 
         await flushed(ctx.message)
         await ctx.send(await get_cat())
+
+    @commands.command(name='dog', aliases=['doggo', 'puppy', 'bork'])
+    @commands.cooldown(1, 30, commands.BucketType.guild)
+    @commands.guild_only()
+    async def dog(self, ctx):
+        '''Bork Bork! 🐶'''
+
+        await flushed(ctx.message)
+        await ctx.send(await get_doggo())
 
 def setup(bot):
     bot.add_cog(Animal(bot))
