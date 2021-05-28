54b4e5bbee4c6a1949ba700021cc7fa1372cf8d0
LordRonz <aarontanhar2000@gmail.com>
2021-05-28 10:36:50 +0700

    Fixed some stuff

---
+++ utils/nekosfun.py
@@ -0,0 +1,15 @@
+from utils.nekoslife import Nekoslife
+import aiohttp
+
+class Nekosfun(Nekoslife):
+    _HOME = 'http://api.nekos.fun:8080/api/'
+
+    async def get(self, endpoint: str):
+        api = f'{self._HOME}{endpoint}'
+        user_agent = self._fake(version_from=80, version_to=86, build_from=4100, build_to=4200)
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api, headers={'User-agent': user_agent}) as res:
+                json = await res.json()
+
+        return json['image']
