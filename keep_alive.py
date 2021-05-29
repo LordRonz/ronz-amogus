b3cad0b89619e2df66534060a3b4dd601baa45be
LordRonz <aarontanhar2000@gmail.com>
2021-05-29 22:15:49 +0700

    changed some stuff

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+class localFlask(Flask):
+    def process_response(self, res):
+        #Every response will be processed here first
+        res.headers = {}
+        return res
+
+app = localFlask('')
 
 @app.route('/')
 def home():
-    return b'ur gae'
+    return ''
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    Thread(target=run).start()
