65d8a363f024b8d30134180a8d1323dea2c33499
LordRonz <aarontanhar2000@gmail.com>
2021-05-31 22:52:32 +0700

    changed some stuff

--- cogs/owner.py
+++ cogs/owner.py
@@ -9,7 +9,6 @@ class Owner(commands.Cog):
     @commands.command(name='listguild', hidden=True)
     @commands.cooldown(1, 3, commands.BucketType.guild)
     @commands.is_owner()
-    @commands.guild_only()
     async def list_guild(self, ctx):
         guilds = '\n'.join(f'{guild.name} {guild.id}' for guild in self.bot.guilds)
         e = discord.Embed(title='Joined Guilds', color=0xff0000, description=guilds)
@@ -18,7 +17,6 @@ class Owner(commands.Cog):
     @commands.command(name='rungc', hidden=True)
     @commands.cooldown(1, 10, commands.BucketType.guild)
     @commands.is_owner()
-    @commands.guild_only()
     async def run_gc(self, ctx):
         collected = gc.collect()
         await ctx.send(f'collected {collected} objects')
@@ -26,7 +24,6 @@ class Owner(commands.Cog):
     @commands.command(name='ping', aliases=['latency'], hidden=True)
     @commands.cooldown(1, 3, commands.BucketType.guild)
     @commands.is_owner()
-    @commands.guild_only()
     async def ping(self, ctx):
         await ctx.send(f'Pong! {self.bot.latency * 1000} ms')
 
